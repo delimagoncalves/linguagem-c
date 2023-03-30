e35a760a9b741a9deafc0079f545d49e4dcec132
Gilson de Lima <delimagoncalves@users.noreply.github.com>
2023-03-30 12:40:00 -0300

    Exemplo main e hello em C

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello, World!\n");
+    //system("read -p \"Pressione enter para sair\" saindo");
+    //system("PAUSE"); Caso esteja utilizando o windows
+    return 0;
+}
