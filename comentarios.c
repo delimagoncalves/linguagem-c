6162ff31df0de1144d87502d19f97e760909d122
Gilson de Lima <delimagoncalves@users.noreply.github.com>
2023-03-30 14:35:31 -0300

    Comentários em C

---
+++ comentarios.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //Comentário de linha
+    printf("Hello!\n");
+    /*
+        Comentário de múltiplas linhas
+        Autor: Gilson de Lima
+    */
+    return 0;
+}
